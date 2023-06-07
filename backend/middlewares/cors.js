f8066bc121026edd3180862a99f691e494eaa66c
Yegor Sidorenko <ye.sidorenko@gmail.com>
2023-06-07 17:19:05 +0300

    cors 3

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,5 +12,14 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  next();
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
 };

e3c5c8e7828680f055dcc0b873f78c25e4727acc
Yegor Sidorenko <ye.sidorenko@gmail.com>
2023-08-26 17:49:42 +0300

    test cors 6

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,15 @@
-const allowedCors = [
-  'https://skavr.nomoredomains.rocks',
-  'http://skavr.nomoredomains.rocks',
-  'localhost:3000',
-  'https://api.skavr.nomoredomains.rocks',
-  'http://api.skavr.nomoredomains.rocks',
-  'https://api.nomoreparties.co',
-  'http://api.nomoreparties.co',
-];
+// const allowedCors = [
+//   'https://skavr.nomoredomains.rocks',
+//   'http://skavr.nomoredomains.rocks',
+//   'localhost:3000',
+//   'https://api.skavr.nomoredomains.rocks',
+//   'http://api.skavr.nomoredomains.rocks',
+//   'https://api.nomoreparties.co',
+//   'http://api.nomoreparties.co',
+// ];
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
 
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
@@ -20,7 +17,6 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Origin', origin);
     return res.end();
   }
 
